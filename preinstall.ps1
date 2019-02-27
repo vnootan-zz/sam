31c34d75029fd2dd7557d66fc4ebfab27aea0706
vnootan <47656200+vnootan@users.noreply.github.com>
2019-02-27 13:03:49 +0530

    Update preinstall.ps1
    Removed

--- preinstall.ps1
+++ preinstall.ps1
@@ -9,9 +9,7 @@ Install-Module -Name Azure,AzureRM -force
 write-host ' installed azure module....'; [datetime]::Now
 
 $s_name = "vinayblob1"
-
 $pass = "ryhREPlM4WP8W6J2c7m0PAR4b3e6+R0fmKSJm8CNjLL6fIl5CR5zDhYGgCUGNhku/bfWrtZEIn1BEpw5PLg76g=="
-
 $ctx = new-azurestoragecontext -StorageAccountName $s_name -StorageAccountKey $pass
 
 write-host 'coping text file from azure blob....'; [datetime]::Now
@@ -21,11 +19,5 @@ write-host ' copied text file from azure blob....'; [datetime]::Now
 write-host ' copying solarwindinstaller  from azure blob....'; [datetime]::Now
 Get-AzureStorageBlobContent -Blob Solarwinds-Orion-SAM-6.6.1-OfflineInstaller.exe  -Container vinay-storage-account-container -Destination C:\Windows\Temp\ -Context $ctx
 write-host ' copied solarwindinstaller  from azure blob....'; [datetime]::Now
-
-$ResourceGroupName = "testResourceGroup"
-$CustVMname = "testvm"
-$customscriptname = "preinstall.ps1"
-
-Remove-AzurermVMCustomScriptExtension -ResourceGroupName $ResourceGroupName -VMName $CustVMname –Name $customscriptname -Force
 
 Stop-Transcript
