1cce25b1989cbe97f8913c22fb472864785e0776
vnootan <47656200+vnootan@users.noreply.github.com>
2019-04-02 19:35:41 +0530

    Update installation_script.ps1

--- installation_script.ps1
+++ installation_script.ps1
@@ -1,2 +1,11 @@
 Start-Transcript -Path C:\testpowershell.Log
+
+write-host ' installing NuGet module....'; [datetime]::Now
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+write-host ' installed NuGet module....'; [datetime]::Now
+
+write-host ' installing azure module....'; [datetime]::Now
+Install-Module -Name Azure,AzureRM -force
+write-host ' installed azure module....'; [datetime]::Now
+
 Stop-Transcript
